bad24c56168c83271649a6147ef57e010cc4e778
nik-kita <nik@kita.github>
2025-04-16 05:08:50 +0200

    add morgan for basic logging

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import express from "express";
 import { bookRouter } from "./routes/book.router";
+import morgan from "morgan";
 
 const app = express();
 app.use(express.json());
+app.use(morgan("dev"));
 
 const PORT = process.env.PORT || 3000;
 
